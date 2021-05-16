f620d9a6aeedaf72abeb8b3b990ceb136113222e
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-05-16 11:39:23 -0600

    Add name and cost properties

--- src/classes/Ingredient.js
+++ src/classes/Ingredient.js
@@ -1,6 +1,8 @@
 class Ingredient {
   constructor(ingredientDetails) {
     this.id = ingredientDetails.id
+    this.name = ingredientDetails.name
+    this.estimatedCostInCents = ingredientDetails.estimatedCostInCents
   }
 }
 module.exports = Ingredient
