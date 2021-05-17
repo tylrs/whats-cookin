e2baea4490dc7ee66dd39fb31a38499c2deaaf5f
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-05-17 16:25:27 -0600

    Add filterByTag method skeleton

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -2,6 +2,12 @@ class RecipeRepository {
   constructor(recipes) { 
     this.recipes = recipes
   }
+  filterByTags(tags) {
+    tags.reduce
+    return this.recipes.filter((recipe) => {
+      return 
+    })
+  }
 }
 
 export default RecipeRepository;
