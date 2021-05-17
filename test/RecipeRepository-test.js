8ec53bf20a07ed8873f6a726d8693c37a0136592
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-05-17 15:18:36 -0600

    Add sampleData from import

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,8 +1,18 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
+import sampleData from '../test/sampleData';
+const data = sampleData.sampleData
 
-describe('Recipe', () => {
+describe.only('Recipe Repository', () => {
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
+  it('Should hold an array of recipes', () => {
+    console.log(data.sampleIngredients.length)
+  })
 })
+
+
+
+
+// recipe1 = new Recipe(sampleRecipe, sampleIngredients);
