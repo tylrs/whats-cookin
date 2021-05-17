18e3837c0c9af02dd7a9b728c044503a552c80ac
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-05-17 15:39:59 -0600

    Add setup for test

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,14 +1,25 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
+import Recipe from '../src/classes/Recipe';
 import sampleData from '../test/sampleData';
 const data = sampleData.sampleData
 
 describe.only('Recipe Repository', () => {
+  let recipes, recipeRepository
+  beforeEach('Setup', () => {
+    const sampleRecipes = data.sampleRecipe
+    const sampleIngredients = data.sampleIngredients
+    recipes = sampleRecipes.map((recipe) => {
+      let newRecipe = new Recipe(recipe, sampleIngredients)
+      return newRecipe
+    })
+    const recipeRepository = new RecipeRepository(recipes)
+  })
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
   it('Should hold an array of recipes', () => {
-    console.log(data.sampleIngredients.length)
+    expect(recipeRepository.recipes).to.deep.equal(recipes)
   })
 })
 
