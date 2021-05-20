ce9dc70fbb05c890aa2347272d263970a17b937c
Taylor Galloway <gallowaytaylor@outlook.com>
2021-05-19 19:47:02 -0500

    Add basic loop to add DOM elements

--- src/scripts.js
+++ src/scripts.js
@@ -4,16 +4,33 @@ import Recipe from './classes/Recipe.js'
 import RecipeRepository from './classes/RecipeRepository.js'
 import sampleData from '../test/sampleData';
 const data = sampleData.sampleData;
-// querySelectors
-const main = document.querySelector('#recipeContainer')
+// const sampleRecipeCard =
+// `
+// <article class="recipe-card flex-row" id="recipeName" >
+//   <img src=`${recipe.image}` alt="cookies"/>
+//   <div class="recipe-card-info flex-row">
+//     <div class="recipe-tag-container flex-column">
+//       <h3>`${recipe.name}` </h3>
+//       <div class="tag-container flex-row">
+//         <h4>Breakfast</h4>
+//         <h4>Lunch</h4>
+//         <h4>Dinner</h4>
+//         <h4>Snack</h4>
+//         <h4>Snack</h4>
+//       </div>
+//     </div>
+//     <div class="recipe-card-buttons-container flex-column">
+//       <button class="favorite-recipe">
+//         <i class="fas fa-heart"></i>
+//       </button>
+//       <button class="this-week-recipe">
+//         <i class="fas fa-calendar-alt"></i>
+//       </button>
+//     </div>
+//   </div>
+// </article>
+// `;
 
-// event listeners
-
-// event handlers
-
-// other functions
-
-console.log('Hello world');
 let recipes, recipeRepository
 const sampleRecipes = data.sampleRecipe
 const sampleIngredients = data.sampleIngredients
@@ -23,6 +40,47 @@ recipes = sampleRecipes.map((recipe) => {
 })
 recipeRepository = new RecipeRepository(recipes)
 console.log(recipeRepository);
+// querySelectors
+const main = document.querySelector('#recipeContainer');
+
+// event listeners
+
+// event handlers
+
+// other functions
+recipeRepository.recipes.forEach((recipe) => {
+  main.innerHTML += `
+  <article class="recipe-card flex-row" id="recipeName" >
+    <img src=${recipe.image}alt="cookies"/>
+    <div class="recipe-card-info flex-row">
+      <div class="recipe-tag-container flex-column">
+        <h3>${recipe.name}</h3>
+        <div class="tag-container flex-row">
+          <h4>Breakfast</h4>
+          <h4>Lunch</h4>
+          <h4>Dinner</h4>
+          <h4>Snack</h4>
+          <h4>Snack</h4>
+        </div>
+      </div>
+      <div class="recipe-card-buttons-container flex-column">
+        <button class="favorite-recipe">
+          <i class="fas fa-heart"></i>
+        </button>
+        <button class="this-week-recipe">
+          <i class="fas fa-calendar-alt"></i>
+        </button>
+      </div>
+    </div>
+  </article>
+  `
+})
+
+
+console.log('Hello world');
+
+
+
 
 // let updatedInstructions, updatedRecipeIngredients, sampleRecipe, sampleIngredients, recipe1, ingredient1, ingredient2, ingredient3;
 //
