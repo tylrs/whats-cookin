1a0bccdc58392e55d62b45fe2c1a2c2ca4b2144a
Taylor Galloway <gallowaytaylor@outlook.com>
2021-05-21 18:10:18 -0500

    Add sample users

--- test/sampleData.js
+++ test/sampleData.js
@@ -655,7 +655,170 @@ const sampleData = {
     '6. Remove the pan from the oven and let sit for 10 minutes before removing onto a cooling rack.Top with ice cream and a drizzle of chocolate sauce.'
   ],
   sampleUsers: [
-    
+    {
+      "name": "Saige O'Kon",
+      "id": 1,
+      "pantry": [
+        {
+          "ingredient": 1002030,
+          "amount": 4
+        },
+        {
+          "ingredient": 12061,
+          "amount": 2
+        },
+        {
+          "ingredient": 19335,
+          "amount": 4
+        },
+        {
+          "ingredient": 15152,
+          "amount": 3
+        },
+        {
+          "ingredient": 9003,
+          "amount": 2
+        },
+        {
+          "ingredient": 18372,
+          "amount": 3
+        },
+        {
+          "ingredient": 2027,
+          "amount": 2
+        }
+      ]
+    },
+    {
+      "name": "Ephraim Goyette",
+      "id": 2,
+      "pantry": [
+        {
+          "ingredient": 1089003,
+          "amount": 2
+        },
+        {
+          "ingredient": 12120,
+          "amount": 2
+        },
+        {
+          "ingredient": 10511282,
+          "amount": 2
+        },
+        {
+          "ingredient": 1019,
+          "amount": 2
+        },
+        {
+          "ingredient": 9302,
+          "amount": 2
+        },
+        {
+          "ingredient": 1011256,
+          "amount": 2
+        },
+        {
+          "ingredient": 9019,
+          "amount": 4
+        },
+        {
+          "ingredient": 11206,
+          "amount": 2
+        },
+        {
+          "ingredient": 19350,
+          "amount": 2
+        },
+        {
+          "ingredient": 9099,
+          "amount": 18
+        },
+        {
+          "ingredient": 14412,
+          "amount": 3
+        }
+      ]
+    },
+    {
+      "name": "Nelda Bosco",
+      "id": 3,
+      "pantry": [
+        {
+          "ingredient": 2049,
+          "amount": 3
+        },
+        {
+          "ingredient": 11282,
+          "amount": 3
+        },
+        {
+          "ingredient": 19334,
+          "amount": 3
+        },
+        {
+          "ingredient": 2031,
+          "amount": 2
+        },
+        {
+          "ingredient": 9302,
+          "amount": 2
+        },
+        {
+          "ingredient": 11463,
+          "amount": 2
+        },
+        {
+          "ingredient": 2025,
+          "amount": 2
+        },
+        {
+          "ingredient": 1002014,
+          "amount": 2
+        },
+        {
+          "ingredient": 2028,
+          "amount": 3
+        },
+        {
+          "ingredient": 4047,
+          "amount": 2
+        }
+      ]
+    },
+    {
+      "name": "Clinton Goodwin",
+      "id": 4,
+      "pantry": [
+        {
+          "ingredient": 99223,
+          "amount": 3
+        },
+        {
+          "ingredient": 93607,
+          "amount": 3
+        },
+        {
+          "ingredient": 11529,
+          "amount": 9
+        },
+        {
+          "ingredient": 14106,
+          "amount": 2
+        },
+        {
+          "ingredient": 2004,
+          "amount": 4
+        },
+        {
+          "ingredient": 12155,
+          "amount": 3
+        },
+        {
+          "ingredient": 1082047,
+          "amount": 2
+        }
+      ]
+    }
   ]
 }
 
