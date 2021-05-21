1a07e171b498fc6267bd4f9c888a1b9a2e2b6432
Taylor Galloway <gallowaytaylor@outlook.com>
2021-05-20 21:05:48 -0500

    Add basic user test

---
+++ test/User-test.js
@@ -0,0 +1,27 @@
+/* eslint-disable max-len */
+import { expect } from 'chai';
+import RecipeRepository from '../src/classes/RecipeRepository';
+import Recipe from '../src/classes/Recipe';
+import sampleData from '../test/sampleData';
+const data = sampleData.sampleData;
+
+describe('Recipe Repository', () => {
+  let recipes, recipeRepository
+  beforeEach('Setup', () => {
+    const sampleRecipes = data.sampleRecipe
+    const sampleIngredients = data.sampleIngredients
+    recipes = sampleRecipes.map((recipe) => {
+      let newRecipe = new Recipe(recipe, sampleIngredients)
+      return newRecipe
+    })
+    recipeRepository = new RecipeRepository(recipes)
+  })
+  it('Should be a function', () => {
+    expect(RecipeRepository).to.be.a('function');
+  });
+
+  it('Should be able to hold recipes', () => {
+    expect(recipeRepository.recipes).to.deep.equal(recipes)
+  })
+
+})
